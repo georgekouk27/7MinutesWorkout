3f32eea4d04850727b62657dec4c2f74211f6f1d
George Koukouris <georgekouk27@gmail.com>
2023-06-21 12:31:12 +0300

    Design start screen

--- app/src/main/java/gr/georkouk/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/gr/georkouk/a7minutesworkout/MainActivity.kt
@@ -2,10 +2,20 @@ package gr.georkouk.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initialize()
     }
+
+    private fun initialize(){
+        val flStartButton: FrameLayout = findViewById(R.id.flStart)
+        flStartButton.setOnClickListener {  }
+    }
+
 }
